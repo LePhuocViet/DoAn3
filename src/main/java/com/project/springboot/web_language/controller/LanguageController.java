106f3cc664051b531564cbb7acbfcc2fefaa4600
LePhuocViet <139203809+LePhuocViet@users.noreply.github.com>
2024-03-16 19:42:43 +0700

    helo

--- src/main/java/com/project/springboot/web_language/controller/LanguageController.java
+++ src/main/java/com/project/springboot/web_language/controller/LanguageController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -26,17 +27,33 @@ public class LanguageController {
     }
 
     @GetMapping("/Home")
-    public String showHome(Model model, Authentication authentication) {
+    public String showHome(Model model, Authentication authentication,HttpServletRequest httpServletRequest, Principal principal) {
+        String search = httpServletRequest.getParameter("search");
         boolean loggedIn = authentication != null && authentication.isAuthenticated();
+        boolean notnull = principal == null;
         model.addAttribute("loggedIn", loggedIn);
-        List<Posts> postslist = postsService.findAll();
-        model.addAttribute("postslist",postslist);
-
+        if(search != null && !search.isEmpty()){
+            List<Posts> posts = postsService.findAll();
+            List<Posts> postsList2 = new ArrayList<>();
+            for(int i = 0 ; i < posts.size();i++){
+                if(posts.get(i).getTieude().contains(search)){
+                    postsList2.add(posts.get(i));
+                }
+            }
+            model.addAttribute("notnull", notnull);
+            model.addAttribute("searchpost",postsList2);
+        } else {
+            List<Posts> postslist = postsService.findAll();
+            model.addAttribute("postslist",postslist);
+        }
         return "home";
+
     }
 
     @GetMapping("/")
     public String detailHome(){
         return "redirect:/Home";
     }
+
+
 }
